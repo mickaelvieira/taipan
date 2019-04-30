3616d952cc834125b2a1c509faf22cca6b48bedb
Mickael Vieira <mickael@mickaelvieira.com>
2019-04-30 15:18:30 +0100

    SQL scripts
    - add script to remove duplicate bookmarks already in the DB
    - add script to create a user because I can never remember the syntax

--- web/sql/delete-bookmark.sql
+++
@@ -1,6 +0,0 @@
-USE `taipan`;
-
--- SELECT @id := id from `bookmarks` where url = "" LIMIT 1;
-DELETE FROM `bookmarks_history` where bookmark_id = @id;
-DELETE FROM `users_bookmarks` where bookmark_id = @id;
-DELETE FROM `bookmarks` where id = @id;
---
+++ web/sql/delete-duplicate-bookmark.sql
@@ -0,0 +1,25 @@
+USE `taipan`;
+
+DELETE FROM `bookmarks_history` where bookmark_id = "40d2d5ca-8817-42d7-9965-791b66c71136";
+DELETE FROM `users_bookmarks` where bookmark_id = "40d2d5ca-8817-42d7-9965-791b66c71136";
+DELETE FROM `bookmarks` where id = "40d2d5ca-8817-42d7-9965-791b66c71136";
+
+DELETE FROM `bookmarks_history` where bookmark_id = "e6519db4-444f-431e-935c-0ec3ea08aedd";
+DELETE FROM `users_bookmarks` where bookmark_id = "e6519db4-444f-431e-935c-0ec3ea08aedd";
+DELETE FROM `bookmarks` where id = "e6519db4-444f-431e-935c-0ec3ea08aedd";
+
+DELETE FROM `bookmarks_history` where bookmark_id = "e2a0d16b-adaa-4fb6-ba71-a36ab78e0fa4";
+DELETE FROM `users_bookmarks` where bookmark_id = "e2a0d16b-adaa-4fb6-ba71-a36ab78e0fa4";
+DELETE FROM `bookmarks` where id = "e2a0d16b-adaa-4fb6-ba71-a36ab78e0fa4";
+
+DELETE FROM `bookmarks_history` where bookmark_id = "53e5fe43-32c7-4e20-8824-16e6ee18d5ce";
+DELETE FROM `users_bookmarks` where bookmark_id = "53e5fe43-32c7-4e20-8824-16e6ee18d5ce";
+DELETE FROM `bookmarks` where id = "53e5fe43-32c7-4e20-8824-16e6ee18d5ce";
+
+DELETE FROM `bookmarks_history` where bookmark_id = "5b214718-1fb5-49fc-89c5-3a6753848f28";
+DELETE FROM `users_bookmarks` where bookmark_id = "5b214718-1fb5-49fc-89c5-3a6753848f28";
+DELETE FROM `bookmarks` where id = "5b214718-1fb5-49fc-89c5-3a6753848f28";
+
+DELETE FROM `bookmarks_history` where bookmark_id = "8617c2d3-ebbf-43b3-b981-8a2fe2ef04a8";
+DELETE FROM `users_bookmarks` where bookmark_id = "8617c2d3-ebbf-43b3-b981-8a2fe2ef04a8";
+DELETE FROM `bookmarks` where id = "8617c2d3-ebbf-43b3-b981-8a2fe2ef04a8";
