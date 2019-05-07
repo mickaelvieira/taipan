ec44d5bd5aedcfd1d792f0a209f5c7e72ab8c90a
Mickael Vieira <mickael@mickaelvieira.com>
2019-05-07 12:22:36 +0100

    Make old ids fields nulable

---
+++ web/sql/migrations/10_make-old-ids-nullable_down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `bookmarks` MODIFY `old_id` CHAR(36) COLLATE utf8mb4_unicode_520_ci NOT NULL;
+ALTER TABLE `users` MODIFY `old_id` CHAR(36) COLLATE utf8mb4_unicode_520_ci NOT NULL;
+ALTER TABLE `users_bookmarks` MODIFY `old_bookmark_id` CHAR(36) COLLATE utf8mb4_unicode_520_ci NOT NULL;
+ALTER TABLE `bookmarks_history` MODIFY `old_bookmark_id` CHAR(36) COLLATE utf8mb4_unicode_520_ci NOT NULL;
+ALTER TABLE `users_bookmarks` MODIFY `old_user_id` CHAR(36) COLLATE utf8mb4_unicode_520_ci NOT NULL;
