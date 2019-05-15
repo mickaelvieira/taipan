acd3cd48a44c2c737ea118b8e90ad93360058fc7
Mickael Vieira <mickael@mickaelvieira.com>
2019-05-15 15:28:48 +0100

    Add missing SQL commands

--- web/sql/migrations/14_link-feeds-to-documents_up.sql
+++ web/sql/migrations/14_link-feeds-to-documents_up.sql
@@ -1,2 +1,4 @@
+DROP TABLE `users_feeds`;
+DELETE FROM `feeds`;
 ALTER TABLE `feeds` ADD COLUMN `document_id` int(11) NOT NULL AFTER `id`;
 ALTER TABLE `feeds` ADD CONSTRAINT `feeds_fk_document_id` FOREIGN KEY (`document_id`) REFERENCES `documents` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
