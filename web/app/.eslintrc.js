6de0946a64bc344b2baff5e55428610e53831a78
Mickael Vieira <mickael@mickaelvieira.com>
2019-06-03 17:12:17 +0100

    Change eslint-plugin-graphql config

--- web/app/.eslintrc.js
+++ web/app/.eslintrc.js
@@ -26,18 +26,10 @@ module.exports = {
     "graphql/template-strings": ["error", {
       // Import default settings for your GraphQL client. Supported values:
       // "apollo", "relay", "lokka", "fraql", "literal"
-      env: "literal",
+      env: "apollo",
 
       // Import your schema JSON here
       schemaJson: require("./schema.json"),
-
-      // OR provide absolute path to your schema JSON (but not if using `eslint --cache`!)
-      // schemaJsonFilepath: path.resolve(__dirname, "./schema.json"),
-
-      // OR provide the schema in the Schema Language format
-      // schemaString: printSchema(schema),
-
-      // tagName is set automatically
     }],
     "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
     "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
