c6d2e2804834897727718bbf39ff625edc023db4
Mickael Vieira <mickael@mickaelvieira.com>
2019-06-20 12:47:44 +0100

    Change unbookmark button icon

--- web/app/js/components/ui/Feed/Button/Unbookmark.tsx
+++ web/app/js/components/ui/Feed/Button/Unbookmark.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/DeleteRounded";
+import DeleteIcon from "@material-ui/icons/DeleteOutline";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { Bookmark } from "../../../../types/bookmark";
 import { Document } from "../../../../types/document";
